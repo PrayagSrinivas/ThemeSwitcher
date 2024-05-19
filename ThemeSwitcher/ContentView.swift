0b244c517d9fc90eb06793355c3530738180fcdf
Srinivas prayag sahu <78862797+PrayagSrinivas@users.noreply.github.com>
2024-05-19 09:44:31 +0530

    Initial Commit

---
+++ ThemeSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThemeSwitcher
+//
+//  Created by Srinivas Prayag Sahu on 19/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThemeSwitcher/ThemeSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeSwitcherApp.swift
+//  ThemeSwitcher
+//
+//  Created by Srinivas Prayag Sahu on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ThemeSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
